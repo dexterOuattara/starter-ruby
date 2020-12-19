0c7204f0cf70e74a328b0c9150342972fa593ae0
dexterOuattara <31624715+dexterOuattara@users.noreply.github.com>
2020-12-19 00:17:29 +0000

    add search
    basic search base on article title

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,9 +12,9 @@ class PostsController < ApplicationController
     cate = params[:cate]
 
     if !cate.nil?
-      @posts = Post.where(:category_id => cate).paginate(page: params[:page], per_page: 10).order('created_at DESC')
+      @posts = Post.where(:category_id => cate).search(params[:search]).paginate(page: params[:page], per_page: 10).order('created_at DESC')
     else
-     @posts = Post.paginate(page: params[:page], per_page: 10)
+     @posts = Post.search(params[:search]).paginate(page: params[:page], per_page: 10)
     end
 
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
     belongs_to :user
     belongs_to :category
     has_one_attached :image
+
+    def self.search(keywords)
+        if keywords.blank?
+            self
+        else
+            self.where('Title Like ?' , "%#{keywords}%")
+        end
+
+    end
 end
